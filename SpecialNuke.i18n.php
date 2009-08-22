51b5f198a790470ffcf17dbf210da13601402c43
Raimond Spekking <raymond@users.mediawiki.org>
2009-08-22 19:30:09 +0000

    Localisation updates for extension messages from translatewiki.net (2009-08-22 19:10 UTC)

--- SpecialNuke.i18n.php
+++ SpecialNuke.i18n.php
@@ -1070,13 +1070,18 @@ $messages['pl'] = array(
 
 /** Piedmontese (Piemontèis)
  * @author Bèrto 'd Sèra
+ * @author Dragonòt
  */
 $messages['pms'] = array(
 	'nuke' => "Scancelament d'amblé",
+	'nuke-desc' => "A dà a j'aministrador l'abilitassion a [[Special:Nuke|scanselé a baron]] le pàgine",
 	'nuke-nopages' => "Gnun-a pàgine faite da [[Special:Contributions/$1|$1]] ant j'ùltim cambiament.",
 	'nuke-list' => "Ste pàgine-sì a son staite faite ant j'ùltim temp da [[Special:Contributions/$1|$1]]; ch'a lassa un coment e ch'a-i daga 'n colp ansima al boton për gaveje via tute d'amblé.",
 	'nuke-defaultreason' => "Scancelament d'amblé dle pàgine faite da $1",
 	'nuke-tools' => "St'utiss-sì a lassa scancelé d'amblé le pàgine gionta ant j'ùltim temp da un chèich utent ò da 'nt na chèich adrëssa IP. Ch'a buta lë stranòm ò l'adrëssa IP për tiré giù na lista dle pàgine da scancelé:",
+	'nuke-submit-user' => 'Va',
+	'nuke-submit-delete' => 'Scansela le selessionà',
+	'right-nuke' => 'Scansela le pàgine a baron',
 );
 
 /** Pashto (پښتو)
