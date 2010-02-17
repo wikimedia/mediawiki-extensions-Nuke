19fc2a3985511984c33a188f3acdaa564dcb7574
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-02-17 20:52:27 +0000

    Localisation updates for special page aliases for extensions from translatewiki.net

--- SpecialNuke.alias.php
+++ SpecialNuke.alias.php
@@ -14,131 +14,123 @@ $aliases['en'] = array(
 	'Nuke' => array( 'Nuke' ),
 );
 
-/** Arabic (العربية)
- * @author Meno25
- */
 $aliases['ar'] = array(
-	'Nuke' => array( 'حذف_كمي' ),
+	'Nuke'   => array( 'حذف_كمي' ),
 );
 
-/** Egyptian Spoken Arabic (مصرى)
- * @author Meno25
- */
 $aliases['arz'] = array(
-	'Nuke' => array( 'حذف_كمى' ),
+	'Nuke'   => array( 'حذف_كمى' ),
 );
 
-/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца)) */
 $aliases['be-tarask'] = array(
-	'Nuke' => array( 'Масавае_выдаленьне' ),
+	'Nuke'   => array( 'Масавае_выдаленьне' ),
 );
 
-/** German (Deutsch)
- * @author Raimond Spekking
- */
 $aliases['de'] = array(
-	'Nuke' => array( 'Massenlöschung' ),
+	'Nuke'   => array( 'Massenlöschung' ),
 );
 
-/** Lower Sorbian (Dolnoserbski) */
 $aliases['dsb'] = array(
-	'Nuke' => array( 'Wulašowanje z kopicami' ),
+	'Nuke'   => array( 'Wulašowanje z kopicami' ),
 );
 
-/** Persian (فارسی) */
+$aliases['et'] = array(
+	'Nuke'   => array( 'Lauskustutamine' ),
+);
+
 $aliases['fa'] = array(
-	'Nuke' => array( 'حذف_دسته‌جمعی' ),
+	'Nuke'   => array( 'حذف_دسته‌جمعی' ),
 );
 
-/** Finnish (Suomi) */
 $aliases['fi'] = array(
-	'Nuke' => array( 'Massapoistaminen' ),
+	'Nuke'   => array( 'Massapoistaminen' ),
 );
 
-/** Swiss German (Alemannisch) */
 $aliases['gsw'] = array(
-	'Nuke' => array( 'Masseleschig' ),
+	'Nuke'   => array( 'Masseleschig' ),
 );
 
-/** Hawaiian (Hawai`i) */
 $aliases['haw'] = array(
-	'Nuke' => array( 'Luku' ),
+	'Nuke'   => array( 'Luku' ),
 );
 
-/** Hebrew (עברית)
- * @author Rotem Liss
- */
 $aliases['he'] = array(
-	'Nuke' => array( 'מחיקה_מרובה' ),
+	'Nuke'   => array( 'מחיקה_מרובה' ),
 );
 
-/** Upper Sorbian (Hornjoserbsce) */
 $aliases['hsb'] = array(
-	'Nuke' => array( 'Masowe wušmórnjenje' ),
+	'Nuke'   => array( 'Masowe wušmórnjenje' ),
 );
 
-/** Hungarian (Magyar) */
 $aliases['hu'] = array(
-	'Nuke' => array( 'Kiírtás' ),
+	'Nuke'   => array( 'Kiírtás' ),
 );
 
-/** Interlingua (Interlingua) */
 $aliases['ia'] = array(
-	'Nuke' => array( 'Deler in massa' ),
+	'Nuke'   => array( 'Deler in massa' ),
 );
 
-/** Japanese (日本語) */
+$aliases['it'] = array(
+	'Nuke'   => array( 'CancellazioneMassiva' ),
+);
+
 $aliases['ja'] = array(
-	'Nuke' => array( 'まとめて削除' ),
+	'Nuke'   => array( 'まとめて削除' ),
 );
 
-/** Korean (한국어) */
 $aliases['ko'] = array(
-	'Nuke' => array( '문서대량삭제' ),
+	'Nuke'   => array( '문서대량삭제' ),
 );
 
-/** Ripoarisch (Ripoarisch) */
 $aliases['ksh'] = array(
-	'Nuke' => array( 'Maßesch Sigge Fottschmiiße' ),
+	'Nuke'   => array( 'Maßesch Sigge Fottschmiiße' ),
 );
 
-/** Maltese (Malti) */
-$aliases['mt'] = array(
-	'Nuke' => array( 'Nuke' ),
+$aliases['lb'] = array(
+	'Nuke'   => array( 'Masseläschung' ),
 );
 
-/** Nedersaksisch (Nedersaksisch) */
+$aliases['mk'] = array(
+	'Nuke'   => array( 'Нукирање' ),
+);
+
+$aliases['ml'] = array(
+	'Nuke'   => array( 'സർവ്വനാശം' ),
+);
+
 $aliases['nds-nl'] = array(
-	'Nuke' => array( 'Massaal_vortdoon' ),
+	'Nuke'   => array( 'Massaal_vortdoon' ),
 );
 
-/** Dutch (Nederlands) */
 $aliases['nl'] = array(
-	'Nuke' => array( 'MassaalVerwijderen' ),
+	'Nuke'   => array( 'MassaalVerwijderen' ),
 );
 
-/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬) */
+$aliases['nn'] = array(
+	'Nuke'   => array( 'Massesletting' ),
+);
+
 $aliases['no'] = array(
-	'Nuke' => array( 'Massesletting' ),
+	'Nuke'   => array( 'Massesletting' ),
 );
 
-/** Polish (Polski) */
 $aliases['pl'] = array(
-	'Nuke' => array( 'Masowe usuwanie' ),
+	'Nuke'   => array( 'Masowe usuwanie' ),
 );
 
-/** Sanskrit (संस्कृत) */
 $aliases['sa'] = array(
-	'Nuke' => array( 'न्यूक' ),
+	'Nuke'   => array( 'न्यूक' ),
 );
 
-/** Slovak (Slovenčina) */
 $aliases['sk'] = array(
-	'Nuke' => array( 'Zničenie' ),
+	'Nuke'   => array( 'Zničenie' ),
 );
 
-/** Tagalog (Tagalog) */
 $aliases['tl'] = array(
-	'Nuke' => array( 'Malawakang pagbura' ),
+	'Nuke'   => array( 'Malawakang pagbura' ),
+);
+
+$aliases['tr'] = array(
+	'Nuke'   => array( 'Uçur' ),
 );
 
