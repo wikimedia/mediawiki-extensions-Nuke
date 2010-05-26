4eef8b25fd55ae3752e8cedf02d2b45bccd87a1c
Chad Horohoe <demon@users.mediawiki.org>
2010-05-26 22:25:32 +0000

    Large commit. Removed 800+ references to no-op wfLoadExtensionMessages()

--- Nuke_body.php
+++ Nuke_body.php
@@ -5,7 +5,6 @@ if( !defined( 'MEDIAWIKI' ) )
 
 class SpecialNuke extends SpecialPage {
 	function __construct() {
-		wfLoadExtensionMessages( 'Nuke' );
 		parent::__construct( 'Nuke', 'nuke' );
 	}
 
