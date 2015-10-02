afb4e332a90e1c0b3a2d68eedf03f8541005673f
Siebrand Mazeland <siebrand@kitano.nl>
2015-10-02 09:48:42 +0200

    Add tests
    * Fix all issues so tests pass.
    * Rework deprecated functions. Compatibility is now with
      MediaWiki 1.23 or later.
    
    Change-Id: I08a0be7df48f9a39951cdc4edd4091fd4b89eade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,19 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
 	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js'
+			]
+		},
 		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
@@ -14,6 +23,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
