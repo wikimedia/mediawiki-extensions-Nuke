95bc436d9fa5493bb237e7031d8902b9b57441c5
Amir Sarabadani <ladsgroup@gmail.com>
2018-01-21 21:56:31 +0100

    Fix wrong use of mw.msg
    See https://www.mediawiki.org/wiki/Manual:Messages_API#mw.msg
    
    Bug: T131975
    Change-Id: Ib8aa253cb1f994c7abf8939c649a488411455e2f

--- ext.nuke.confirm.js
+++ ext.nuke.confirm.js
@@ -6,7 +6,7 @@
 		$( 'form[name="nukelist"]' ).on( 'submit', function () {
 			var pages = $( this ).find( 'input[name="pages[]"][type="checkbox"]:checked' );
 			if ( pages.length ) {
-				return confirm( mw.msg( 'nuke-confirm', pages.length ).text() );
+				return confirm( mw.msg( 'nuke-confirm', pages.length ) );
 			}
 		} );
 
